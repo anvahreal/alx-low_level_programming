d380c93d542a4dc8e8578b77ffd0e6fcc3c9b903
anvahreal <marvellouskayode17@gmail.com>
2022-12-16 04:43:21 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * 
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	_putchar('\n');
+}
