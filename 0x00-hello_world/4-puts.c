1125908bba7f4bfec8f05026a85460bcddc26e51
anvahreal <marvellouskayode17@gmail.com>
2022-12-16 01:12:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-
 #include <stdio.h>
 
 /**
  * main - print the string in the put function
  *
- * Description: using the main function
- * this program prints 'Programmin is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0
  */
  int main(void)
 {
