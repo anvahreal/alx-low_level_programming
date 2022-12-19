0f3f25f6cec01ba0a0fc52d4ba6bc951f10a1929
anvahreal <marvellouskayode17@gmail.com>
2022-12-19 12:12:23 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
  * reset_to_98 - check the code
- *
+ * @n: declaration of n and paramters for the function reset_to_98
  * Return: Always 0.
  */
 void reset_to_98(int *n)
