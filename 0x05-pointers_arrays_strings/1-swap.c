cbc1d4c68d36ae76362bb6a8d5cdc006df0430c2
anvahreal <marvellouskayode17@gmail.com>
2022-12-19 12:14:44 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * swap_int - check the code.
- * 
+ * swap_int - check the code
+ * @a: declaration of a and paramters for the function swap_int
+ * @b: declaration of b and paramters for the function swap_int
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
