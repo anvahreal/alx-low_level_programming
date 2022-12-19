d51f1b84138f69f7fb0baebee063913b4ca78cc2
anvahreal <marvellouskayode17@gmail.com>
2022-12-19 12:27:12 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * main - check the code.
- * 
+ * print_rev - func that prints a string, in reverse, followed by a new line
+ * @s: declaration of s and paramters for the function
  * Return: Always 0.
  */
 void print_rev(char *s)
