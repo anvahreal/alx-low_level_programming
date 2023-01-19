3d7eafe1f550ae7343fe68e027306380a4c7c12a
anvahreal <marvellouskayode17@gmail.com>
2023-01-19 06:15:11 -0800

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,15 +1,16 @@
-#ifndef main_H_FILE
-#define main_H_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int helper(int n, int i);
 int is_prime_number(int n);
-int helper_prime(int n, int i, int limit);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
 
 #endif
